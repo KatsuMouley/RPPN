f54dc9fdd3102a80f3dfb5cbcfdd9519b8ea9236
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-05-27 00:19:41 -0300

    deleting test

--- test/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('toggleButton');
-    const redBox = document.getElementById('redBox');
-
-    toggleButton.addEventListener('click', function() {
-        redBox.classList.toggle('visible');
-    });
-});
